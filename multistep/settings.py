73374ba4809890904fed98a344ed29301939d520
Olawoyin <yustee2017@gmail.com>
2025-04-02 09:35:30 +0100

    Setup the static files for deployment

--- multistep/settings.py
+++ multistep/settings.py
@@ -56,6 +56,9 @@ MIDDLEWARE = [
     
     # cors headers
     "corsheaders.middleware.CorsMiddleware",
+    
+    #whiteNoise
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'multistep.urls'
@@ -138,7 +141,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
